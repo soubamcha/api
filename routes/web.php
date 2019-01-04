8ab43ed763c97ad29db93a983eddfc1f2855feb1
Z <training@localhost.localdomain>
2019-01-04 16:54:56 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    // return $router->app->version();
+    return response()->json(['message' => $router->app->version()], 200, array(), JSON_PRETTY_PRINT);
+});
+
+$router->get('/time', function () use ($router) {
+    return response()->json(['message' => date('Y-m-d H:i:s T')], 200, array(), JSON_PRETTY_PRINT);
+});
+
+$router->get('/hello', function () use ($router) {
+    return response()->json(['message' => "Hello World!"], 200, array(), JSON_PRETTY_PRINT);
+});
